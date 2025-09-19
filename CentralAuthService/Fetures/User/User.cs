11efcf6b625e0165592d9a5899339f595cd9f6a8
Seif357 <seifomar3579@gmail.com>
2025-09-19 12:07:21 +0300

    init

---
+++ CentralAuthService/Fetures/User/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+
+public class User : IdentityUser<int>
+
+{
+}
