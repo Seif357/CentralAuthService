82f740707253baed99cc1a2c455262990d6bd506
Seif357 <seifomar3579@gmail.com>
2025-09-20 17:54:10 +0300

    Trying GitHub Actions

--- CentralAuthService/Fetures/User/User.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNetCore.Identity;
-
-public class User : IdentityUser<int>
-
-{
-}
